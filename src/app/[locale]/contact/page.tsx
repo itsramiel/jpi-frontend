fc00ec78977100f2a5a7ea03557c71aafdaf8804
ramiel <ramielwan48@gmail.com>
2023-08-12 19:52:12 +0300

    feat: add arabic translations

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,29 +1,31 @@
 "use client";
 
 import { Button, Input, TextAreaInput } from "@/components";
+import { useTranslations } from "next-intl";
 import { Controller, useForm } from "react-hook-form";
 import { IoCallSharp, IoMailSharp } from "react-icons/io5";
 
 export default function Contact() {
+  const t = useTranslations("conatct");
+
   return (
     <div className="flex flex-col lg:flex-row gap-8 my-16">
       <main className="flex flex-1 flex-col gap-8">
         <div className="flex flex-col gap-2">
-          <h1 className="text-gray-950 text-4xl font-bold">Get in touch</h1>
-          <h2 className="text-gray-600 font-medium">
-            We’d love to answer any questions or inquiries to help you in your
-            investment journey
-          </h2>
+          <h1 className="text-gray-950 text-4xl font-bold">
+            {t("form.title")}
+          </h1>
+          <h2 className="text-gray-600 font-medium">{t("form.description")}</h2>
         </div>
         <Form />
       </main>
       <aside className="flex flex-1 flex-col gap-4">
         <div className="flex flex-col gap-1">
           <h1 className="text-gray-950 text-4xl font-bold">
-            Or reach us directly
+            {t("ourContacts.title")}
           </h1>
           <h2 className="text-gray-600 font-medium">
-            Feel free to contact us yourself by:
+            {t("ourContacts.description")}
           </h2>
         </div>
         <ul className="flex flex-col gap-1">
@@ -43,6 +45,7 @@ export default function Contact() {
 }
 
 function Form() {
+  const t = useTranslations("conatct.form");
   const { control, handleSubmit } = useForm({
     defaultValues: {
       email: "",
@@ -54,25 +57,25 @@ function Form() {
   return (
     <form
       className="flex flex-col gap-8"
-      onSubmit={handleSubmit((data) => {
-        console.log(data);
+      onSubmit={handleSubmit(() => {
+        // add logic to send message once we found how we'll do that
       })}
     >
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 max-w-sm">
         <Controller
           name="email"
           control={control}
           rules={{
-            required: "This field is required",
+            required: t("inputs.errors.required"),
             pattern: {
               value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-              message: "Please enter a valid email",
+              message: t("inputs.errors.invalidEmail"),
             },
           }}
           render={({ field, fieldState: { error } }) => (
             <Input
               {...field}
-              fieldLabel={"Email"}
+              fieldLabel={t("inputs.labels.email")}
               required
               error={error?.message}
             />
@@ -85,13 +88,13 @@ function Form() {
             pattern: {
               value:
                 /^(\+?\d{1,3}[-.\s]?)?(\(?\d{1,5}?\)?[-.\s]?)?\d{1,5}[-.\s]?\d{1,9}$/g,
-              message: "Please enter a valid phone number",
+              message: t("inputs.errors.invalidPhoneNumber"),
             },
           }}
           render={({ field, fieldState: { error } }) => (
             <Input
               {...field}
-              fieldLabel="Phone Number"
+              fieldLabel={t("inputs.labels.phoneNumber")}
               error={error?.message}
             />
           )}
@@ -100,19 +103,19 @@ function Form() {
           name="message"
           control={control}
           rules={{
-            required: "This field is required",
+            required: t("inputs.errors.required"),
           }}
           render={({ field, fieldState: { error } }) => (
             <TextAreaInput
               {...field}
-              fieldLabel={"Message"}
+              fieldLabel={t("inputs.labels.message")}
               required
               error={error?.message}
             />
           )}
         />
       </div>
-      <Button className="self-start">Send Message</Button>
+      <Button className="self-start">{t("submitBtn")}</Button>
     </form>
   );
 }
