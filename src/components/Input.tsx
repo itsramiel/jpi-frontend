d6ad6bf3f81b3804ff7bd6f0807bbf63357dc7cf
ramiel <ramielwan48@gmail.com>
2023-08-31 22:30:25 +0300

    fix: fix inputs for password managers

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,11 +9,15 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ fieldLabel, required, error, ...props }, forwardedRef) => {
     return (
       <BaseInput fieldLabel={fieldLabel} required={required} error={error}>
-        <input
-          ref={forwardedRef}
-          {...props}
-          className="py-3 px-2 rounded-lg border border-gray-400"
-        />
+        {/* The extra div is just for the sake of password managers which add an extra element on top of input. If this container 
+      has a gap, then there will be an extra space*/}
+        <div>
+          <input
+            ref={forwardedRef}
+            {...props}
+            className="py-3 px-2 rounded-lg border border-gray-400"
+          />
+        </div>
       </BaseInput>
     );
   }
