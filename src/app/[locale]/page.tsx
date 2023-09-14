23d08be7825e9727c96cebf6341813c71d01544e
ramiel <ramielwan48@gmail.com>
2023-09-14 07:30:48 +0300

    feat: make homepage responsive

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -14,106 +14,17 @@ import {
 export default function Home() {
   return (
     <div className="px-4 py-9 flex flex-col gap-9">
-      <div className="flex flex-col gap-8 items-center">
-        <div className="flex flex-col gap-4">
-          <h2 className="text-gray-900 text-center text-4xl font-semibold">
-            Invest in Northern Cyprus Real Estate like a Professional
-          </h2>
-          <p className="text-gray-600 text-center text-base font-medium">
-            {
-              "Jokanda Property Investment empowers investors with clarity and analytics in Northern Cyprus real estate. We're your guide to informed investing."
-            }
-          </p>
-        </div>
-        <div className="flex flex-col gap-2 self-stretch">
-          <div className="bg-red-600 w-full aspect-[3/2] rounded"></div>
-          <div className="flex gap-2">
-            {Array(5)
-              .fill(0)
-              .map((_, index) => (
-                <div
-                  className={`h-1 flex-1 rounded-[1px] ${
-                    index === 0 ? "bg-gray-800" : "bg-gray-800/30"
-                  }`}
-                  key={index}
-                ></div>
-              ))}
-          </div>
-        </div>
-        <Button trailingIcon={IoArrowForwardSharp}>
-          Our Premium Property Selection
-        </Button>
-      </div>
+      <InvestInProjects />
       <Seperator />
-      <div className="flex flex-col items-center gap-8">
-        <div className="flex flex-col gap-4">
-          <h2 className="text-gray-900 text-center text-4xl font-semibold">
-            Discover Your Next Investment and Paradise
-          </h2>
-          <p className="text-gray-600 text-base font-medium text-center">
-            From profit-driven real estate to peaceful living, Northern Cyprus
-            offers the best of both worlds.
-          </p>
-        </div>
-        <div className="flex flex-col gap-2 self-stretch">
-          <div className="bg-red-600 w-full aspect-[3/2] rounded"></div>
-          <div className="flex gap-2">
-            {Array(5)
-              .fill(0)
-              .map((_, index) => (
-                <div
-                  className={`h-1 flex-1 rounded-[1px] ${
-                    index === 0 ? "bg-gray-800" : "bg-gray-800/30"
-                  }`}
-                  key={index}
-                ></div>
-              ))}
-          </div>
-        </div>
-      </div>
+      <ExploreCountry />
       <Seperator />
-      <div className="flex flex-col gap-8 items-center">
-        <div className="flex flex-col items-center gap-4">
-          <h2 className="text-gray-900 text-center text-4xl font-semibold">
-            Why choose Jokanda?
-          </h2>
-          <div className="flex flex-col gap-4">
-            <div className="flex gap-2">
-              <SellingPoint
-                icon={IoBarChartOutline}
-                title="Data-Driven Insights."
-                subtitle="Beyond the surface with deep-dive numbers to guide your investments."
-              />
-              <SellingPoint
-                icon={IoMapOutline}
-                title="Local Expertise."
-                subtitle="An adept understanding of the Northern Cyprus market with the latest trends at our fingertips."
-              />
-            </div>
-            <div className="flex gap-2">
-              <SellingPoint
-                icon={IoBulbOutline}
-                title="Personalized Guidance."
-                subtitle="Tailored recommendations to align with your unique investment goals."
-              />
-              <SellingPoint
-                icon={IoSearchOutline}
-                title="Transparency."
-                subtitle="Complete clarity every step of your journey, ensuring informed decisions."
-              />
-            </div>
-          </div>
-        </div>
-        <Button trailingIcon={IoArrowForwardSharp}>
-          Schedule a Free Consultation
-        </Button>
-      </div>
+      <SellingPoints />
       <Seperator />
       <div className="flex flex-col gap-4">
         <h2 className="text-gray-900 text-center text-2xl font-medium">
           Partnering with the pioneers of Northern Cyprus Construction
         </h2>
-        <div className="flex flex-row justify-center gap-4 items-center">
+        <div className="flex flex-row flex-wrap justify-center gap-4 items-center">
           <Image
             src={require("@/../public/partners/dovec-logo-light.png")}
             alt="dovec logo"
@@ -132,6 +43,115 @@ export default function Home() {
 
 function Seperator() {
   return <hr className="border-gray-950/10 border" />;
+}
+
+function InvestInProjects() {
+  return (
+    <div className="flex flex-col md:flex-row gap-9 items-center">
+      <div className="md:flex-1 flex flex-col gap-6 items-center">
+        <div className="flex flex-col gap-4">
+          <h2 className="text-gray-900 text-center text-4xl font-semibold">
+            Invest in Northern Cyprus Real Estate like a Professional
+          </h2>
+          <p className="text-gray-600 text-center text-base font-medium">
+            {
+              "Jokanda Property Investment empowers investors with clarity and analytics in Northern Cyprus real estate. We're your guide to informed investing."
+            }
+          </p>
+        </div>
+        <Button trailingIcon={IoArrowForwardSharp}>
+          Our Premium Property Selection
+        </Button>
+      </div>
+      <div className="md:flex-1 flex flex-col gap-2 self-stretch">
+        <div className="bg-red-600 w-full aspect-[3/2] rounded"></div>
+        <div className="flex gap-2">
+          {Array(5)
+            .fill(0)
+            .map((_, index) => (
+              <div
+                className={`h-1 flex-1 rounded-[1px] ${
+                  index === 0 ? "bg-gray-800" : "bg-gray-800/30"
+                }`}
+                key={index}
+              ></div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ExploreCountry() {
+  return (
+    <div className="flex flex-col md:flex-row-reverse items-center gap-9">
+      <div className="md:flex-1 flex flex-col gap-4">
+        <h2 className="text-gray-900 text-center text-4xl font-semibold">
+          Discover Your Next Investment and Paradise
+        </h2>
+        <p className="text-gray-600 text-center text-base font-medium">
+          From profit-driven real estate to peaceful living, Northern Cyprus
+          offers the best of both worlds.
+        </p>
+      </div>
+      <div className="md:flex-1 flex flex-col gap-2 self-stretch">
+        <div className="bg-red-600 w-full aspect-[3/2] rounded"></div>
+        <div className="flex gap-2">
+          {Array(5)
+            .fill(0)
+            .map((_, index) => (
+              <div
+                className={`h-1 flex-1 rounded-[1px] ${
+                  index === 0 ? "bg-gray-800" : "bg-gray-800/30"
+                }`}
+                key={index}
+              ></div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function SellingPoints() {
+  return (
+    <div className="flex flex-col gap-8 items-center">
+      <div className="flex flex-col items-center gap-4">
+        <h2 className="text-gray-900 text-center text-4xl font-semibold">
+          Why choose Jokanda?
+        </h2>
+        <div className="flex flex-col gap-4 max-w-3xl">
+          <div className="flex gap-2">
+            <SellingPoint
+              icon={IoBarChartOutline}
+              title="Data-Driven Insights."
+              subtitle="Beyond the surface with deep-dive numbers to guide your investments."
+            />
+            <SellingPoint
+              icon={IoMapOutline}
+              title="Local Expertise."
+              subtitle="An adept understanding of the Northern Cyprus market with the latest trends at our fingertips."
+            />
+          </div>
+          <div className="flex gap-2">
+            <SellingPoint
+              icon={IoBulbOutline}
+              title="Personalized Guidance."
+              subtitle="Tailored recommendations to align with your unique investment goals."
+            />
+            <SellingPoint
+              icon={IoSearchOutline}
+              title="Transparency."
+              subtitle="Complete clarity every step of your journey, ensuring informed decisions."
+            />
+          </div>
+        </div>
+      </div>
+      <Button trailingIcon={IoArrowForwardSharp}>
+        Schedule a Free Consultation
+      </Button>
+    </div>
+  );
 }
 
 interface SellingPointProps {
