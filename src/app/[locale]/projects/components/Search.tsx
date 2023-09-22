1079b5c49989a68d2183259a53676a4ceea90665
ramiel <ramielwan48@gmail.com>
2023-09-22 18:56:18 +0300

    feat: add dummy filters for now

--- src/app/[locale]/projects/components/Search.tsx
+++ src/app/[locale]/projects/components/Search.tsx
@@ -1,10 +1,12 @@
 "use client";
+/*eslint i18next/no-literal-string: 0*/
 
 import { usePathname, useRouter } from "next-intl/client";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IoSearchOutline } from "react-icons/io5";
 import { useDebounce } from "use-debounce";
+import { Select, SelectItem, SelectSeparator } from "./Select";
 
 export function Search() {
   const router = useRouter();
@@ -12,6 +14,8 @@ export function Search() {
 
   const pathName = usePathname();
   const [search, setSearch] = useState(searchParams.get("search") ?? "");
+  const [bedroomCount, setBedroomCount] = useState("");
+  const [propertyType, setPropertyType] = useState("");
   const [debouncedSearch] = useDebounce(search, 750);
 
   useEffect(() => {
@@ -25,18 +29,67 @@ export function Search() {
       }
       router.push(`${pathName}${params ? `?${params}` : ""}`);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [debouncedSearch]);
 
+  const onBedroomCountChange = (value: string) => {
+    setBedroomCount(value === "_clear" ? "" : value);
+  };
+
+  const onPropertyTypeChange = (value: string) => {
+    setPropertyType(value === "_clear" ? "" : value);
+  };
+
   return (
-    <div className="flex py-3 px-2 gap-8 bg-black/5 items-center rounded">
-      <IoSearchOutline className="w-6 h-6 text-gray-700" />
-      <input
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        type="text"
-        placeholder="Search by project name"
-        className="flex-1 bg-transparent text-gray-800 focus:outline-none placeholder:text-gray-500 placeholder:font-regular"
-      />
+    <div className="flex flex-col gap-2">
+      <div className="flex py-3 px-2 gap-8 bg-zinc-100 items-center rounded">
+        <IoSearchOutline className="w-6 h-6 text-gray-700" />
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Search by project name"
+          className="flex-1 bg-transparent text-gray-800 focus:outline-none placeholder:text-gray-500 placeholder:font-regular"
+        />
+      </div>
+      <div className="flex gap-2">
+        <Select
+          display={`No. of Bedrooms: ${bedroomCount}`}
+          placeholder="No. of Bedrooms"
+          onValueChange={onBedroomCountChange}
+          value={bedroomCount}
+        >
+          {bedroomCount !== "" ? (
+            <>
+              <SelectItem value="_clear">clear</SelectItem>
+              <SelectSeparator />
+            </>
+          ) : null}
+          {["0", "1", "2", "3", "4"].map((item) => (
+            <SelectItem value={item} key={item}>
+              {item}
+            </SelectItem>
+          ))}
+        </Select>
+        <Select
+          display={propertyType}
+          placeholder="Property Type"
+          onValueChange={onPropertyTypeChange}
+          value={propertyType}
+        >
+          {propertyType !== "" ? (
+            <>
+              <SelectItem value="_clear">clear</SelectItem>
+              <SelectSeparator />
+            </>
+          ) : null}
+          {["Apartment", "Villa"].map((item) => (
+            <SelectItem value={item} key={item}>
+              {item}
+            </SelectItem>
+          ))}
+        </Select>
+      </div>
     </div>
   );
 }
---
+++ src/app/[locale]/projects/components/Select.tsx
@@ -0,0 +1,71 @@
+import React, { ComponentRef } from "react";
+import * as RadixSelect from "@radix-ui/react-select";
+import { IoCheckmark, IoChevronDown, IoChevronUp } from "react-icons/io5";
+
+interface FilterProps {
+  placeholder: NonNullable<RadixSelect.SelectValueProps["placeholder"]>;
+  children: React.ReactNode;
+  onValueChange: (value: string) => void;
+  value: string;
+  display: string;
+}
+
+export function Select({
+  children,
+  onValueChange,
+  value,
+  placeholder,
+  display,
+}: FilterProps) {
+  return (
+    <RadixSelect.Root value={value} onValueChange={onValueChange}>
+      <RadixSelect.Trigger className="bg-zinc-100 rounded flex items-center py-2 px-2 gap-2 border-black text-sm text-gray-700 data-[placeholder]:text-gray-500">
+        <RadixSelect.Value placeholder={placeholder}>
+          {display}
+        </RadixSelect.Value>
+        <RadixSelect.Icon>
+          <IoChevronDown className="w-3 h-3" />
+        </RadixSelect.Icon>
+      </RadixSelect.Trigger>
+      <RadixSelect.Portal>
+        <RadixSelect.Content className="bg-zinc-100 rounded shadow-black/20 shadow">
+          <RadixSelect.ScrollUpButton className="flex flex-col items-center text-gray-700">
+            <IoChevronUp />
+          </RadixSelect.ScrollUpButton>
+
+          <RadixSelect.Viewport className="p-1">
+            {children}
+          </RadixSelect.Viewport>
+
+          <RadixSelect.ScrollDownButton className="flex flex-col items-center text-gray-700">
+            <IoChevronDown />
+          </RadixSelect.ScrollDownButton>
+        </RadixSelect.Content>
+      </RadixSelect.Portal>
+    </RadixSelect.Root>
+  );
+}
+
+export const SelectSeparator = () => (
+  <RadixSelect.Separator className="h-[1px] bg-gray-300" />
+);
+
+export const SelectItem = React.forwardRef<
+  ComponentRef<typeof RadixSelect.SelectItem>,
+  RadixSelect.SelectItemProps
+>(({ children, className, ...props }, forwardedRef) => {
+  return (
+    <RadixSelect.Item
+      {...props}
+      ref={forwardedRef}
+      className="flex items-center rounded select-none gap-4 p-2 text-sm text-gray-700 data-[highlighted]:text-gray-100 bg-zinc-100 data-[highlighted]:bg-zinc-900 data-[highlighted]:outline-none focus:outline-none"
+    >
+      <RadixSelect.ItemText>{children}</RadixSelect.ItemText>
+      <RadixSelect.ItemIndicator className="SelectItemIndicator">
+        <IoCheckmark className="w-3 h3" />
+      </RadixSelect.ItemIndicator>
+    </RadixSelect.Item>
+  );
+});
+
+SelectItem.displayName = "SelectItem";
