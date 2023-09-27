fafd04d1b64e1b3392dc4c76aced54180ae5d25d
ramiel <ramielwan48@gmail.com>
2023-09-27 22:40:35 +0300

    feat: add arabic font

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,9 +1,16 @@
 import "../globals.css";
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { Noto_Naskh_Arabic } from "@next/font/google";
+
 import { LocaleSegmentProps } from "./types";
 import { DirectionProvider, Footer, Navbar } from "./components";
 import { AbstractIntlMessages, NextIntlClientProvider } from "next-intl";
+
+const rtlFont = Noto_Naskh_Arabic({
+  subsets: ["arabic"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Home",
@@ -35,7 +42,11 @@ export default async function RootLayout({
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
       <DirectionProvider dir={dir}>
-        <html lang={locale} dir={dir}>
+        <html
+          lang={locale}
+          dir={dir}
+          className={dir === "rtl" ? rtlFont.className : undefined}
+        >
           <body className="min-h-screen flex flex-col">
             <Navbar />
             <div className="w-full max-w-6xl mx-auto px-4 pt-16">
